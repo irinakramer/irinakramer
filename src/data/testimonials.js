9d1a2912855f6d596102a2e9d904fee444a9a4e5
Irina Kramer <irakramer@gmail.com>
2024-04-18 16:19:32 -0700

    Added testimonials

---
+++ src/data/testimonials.js
@@ -0,0 +1,28 @@
+const testimonials = [
+    {
+        author: "Katrina Neill",
+        title: "Senior Copywriter (UX/Marketing)",
+        text: "Irina is the type of professional colleague everyone appreciates and respects. Her combination of tech knowledge, project management and positive collaboration made our sites engaging, clear, highly functional and relevant. ",
+        avatar: "https://media.licdn.com/dms/image/C4E03AQGrrdhY-CCS1Q/profile-displayphoto-shrink_100_100/0/1517747424357?e=1718841600&v=beta&t=4ZP9Fy_PD3pqukPKxxd97K2xiwS7ZFVCECdnvOo7i0k",
+    },
+    // {
+    //     author: "Cherry Yeung",
+    //     title: "Senior Product Designer",
+    //     text: "Having worked with Irina closely on several projects in the same team, I can attest to her exceptional technical abilities and attention to detail.",
+    //     avatar: "https://media.licdn.com/dms/image/C5103AQHfECWr-urPzA/profile-displayphoto-shrink_100_100/0/1516736378819?e=1718841600&v=beta&t=SPOxJa72nI6-2dAwVjF8fsVn3fTsIEaWi5Zp12FNekI",
+    // },
+    {
+        author: "Jeremy Lane",
+        title: "Engineering Leader",
+        text: "Irina has a strong knowledge of modern web development technologies, including JavaScript, HTML, CSS, and other related tools. Moreover, Irina has an eye for detail and always ensures that her code is clean, maintainable, and efficient.",
+        avatar: "https://media.licdn.com/dms/image/C5603AQE126k9f5alkQ/profile-displayphoto-shrink_100_100/0/1655917734696?e=1718841600&v=beta&t=70_GC4_NeYEWgIhns7eTYcUDpeaAIO9tU3yzRcyv-HE",
+    },
+    {
+        author: "Gize Bonilla",
+        title: "Senior Engineering Manager",
+        text: "Irina is passionate professional always seeking to do the best work possible, her experience, way of thinking, patience and skills make her a very talented software engineer. I always feel like she is in control and has a very well designed plan to get the best solution to any problem.",
+        avatar: "https://media.licdn.com/dms/image/C5603AQG1Wm6OF-u4dQ/profile-displayphoto-shrink_100_100/0/1538186858608?e=1718841600&v=beta&t=nkZ9x0b7Gl7Rp8BmiNvTlUKEPU0_o1qBgqyd-lIwXM4",
+    },
+];
+
+export default testimonials;
