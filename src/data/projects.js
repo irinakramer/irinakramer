a5f8e63b2197525bc10c5f2b704e7a4873fe072a
Irina Kramer <irakramer@gmail.com>
2025-02-18 23:04:31 -0800

    added new portfolio item, desriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,13 +13,15 @@ import projBouquet from "../images/projects/bouquet.jpg";
 import ComingSoon from "../images/projects/ComingSoon.svg";
 import dianamajumdarart from "../images/projects/dianamajumdarart.jpg";
 import fibercirclestudio from "../images/projects/fiber_circle_studio.jpg";
+import veraalden from "../images/projects/vera-alden.jpg";
 
 const projects = [
     {
         name: "Fiber Circle Studio",
         url: "https://fibercirclestudio.com/",
-        description: "tbd",
-        tech: "Shopify, Liquid, HTML, CSS",
+        description:
+            "Enhanced the store’s functionality with a structured product taxonomy, filters, and color swatches. Improved discoverability with Google indexing, added back-in-stock notifications, and a required terms acceptance checkbox. Providing ongoing support for smooth operation and scalability.",
+        tech: "Shopify, Liquid, JavaScript, HTML, CSS, Image processing",
         image: fibercirclestudio,
         year: "2025",
         company: "IKON web studio",
@@ -28,17 +30,27 @@ const projects = [
         name: "Diana Majumdar Art eCommerce",
         url: "http://dianamajumdarart.com",
         description:
-            "Responsive templates for product, category, shopping cart pages, tech specs feed, and marketing landing pages.",
-        tech: "Wordpress Elementor, Woocommerce, PHP, CSS",
+            "Designed and developed a fully responsive eCommerce site with a custom layout, sliders, and a masonry grid. Built custom templates for product, category, shopping cart, and marketing pages. Integrated payment methods, contact forms, Mailchimp signup, Instagram feed, and Google tools for search visibility and shopping ads. Added custom features, including red dot indicators for sold products and an artwork inquiry form on product pages.",
+        tech: "Wordpress Elementor, Woocommerce, JavaScript, PHP, HTML, CSS",
         image: dianamajumdarart,
         year: "2024",
+        company: "IKON web studio",
+    },
+    {
+        name: "Artist eCommerce Template website",
+        url: "https://ikonwebstudio.wixstudio.com/vera-alden",
+        description:
+            "Designed a custom template website for a contemporary artist, highlighting new works, categories, exhibitions, and key content to promote their art. Integrated interactive features to display artwork within frames and in room settings.",
+        tech: "Wix Studio",
+        image: veraalden,
+        year: "2025",
         company: "IKON web studio",
     },
     {
         name: "JOBY.com eCommerce",
         url: "http://joby.com/us-en/",
         description:
-            "Responsive templates for product, category, shopping cart pages, tech specs feed, and marketing landing pages.",
+            "Developed responsive templates for product, category, shopping cart, and marketing landing pages, along with a tech specs feed. Built a custom CMS to empower users with easy content management and updates.",
         tech: "Magento, HTML, CSS, PHP, Typo3, Video",
         image: projJoby,
         year: "2019",
@@ -48,7 +60,7 @@ const projects = [
         name: "Lowepro eCommerce",
         url: "https://www.lowepro.com/us-en/",
         description:
-            "Magento eCommerce theme implementation for international brand, product and category pages, shopping cart optimization, all UI elements and rich content.",
+            "Implemented a custom Magento theme for an international brand, developing product and category pages with optimized shopping cart functionality. Designed and refined all UI elements and rich content for a seamless user experience.",
         tech: "Magento, PHP, JavaScript, HTML, CSS, UGC APIs",
         image: projLowepro,
         year: "2019",
@@ -58,7 +70,7 @@ const projects = [
         name: "Bank Statement Analyzer",
         url: "../projects/bsa",
         description:
-            "Full stack app to analyze bank transactions and calculate ratios for loan eligibility",
+            "Built front end architecture for a full stack app to analyze bank transactions and calculate ratios for loan eligibility.",
         tech: "React, Chakra UI, Rest/GraphQL APIs, Flask",
         image: bsa,
         year: "2021 - 2023",
@@ -68,7 +80,7 @@ const projects = [
         name: "Loweprofessionals",
         url: "https://www.lowepro.com/us-en/loweprofessionals/",
         description:
-            "Landing pages built from PSD mockups to functional templates consisting of components.",
+            "Converted PSD mockups into fully functional, component-based templates, ensuring a seamless and responsive user experience.",
         tech: "HTML, SCSS, JavaScript, Typo3 Flux and Fluid templating",
         image: projAmbassadors,
         year: "2019",
@@ -78,7 +90,7 @@ const projects = [
         name: "Bouquet",
         url: "https://bouquet-topaz.vercel.app/",
         description:
-            "This app is a guide to help you identify aromas and scents present in most wine families. Full CRUD app.",
+            "Developed a full CRUD application designed to help users identify aromas and scents commonly found in various wine families. Provides an interactive and intuitive guide for wine enthusiasts to explore and learn about different fragrance profiles.",
         tech: "React, Material UI, GeoCharts API, Back4App",
         image: projBouquet,
         year: "2021 - 2023",
@@ -113,16 +125,16 @@ const projects = [
     //     ],
     //     company: "Vitec",
     // },
-    {
-        name: "Lowepro Quickfinder",
-        url: "https://quickfinder.netlify.app/",
-        description:
-            "App for quick navigation to a product series page on eCommerce site.",
-        tech: "JavaScript, HTML, CSS, SVG, Typo3 Flux.",
-        image: projQuickfinder,
-        year: "2017",
-        company: "Daymen",
-    },
+    // {
+    //     name: "Lowepro Quickfinder",
+    //     url: "https://quickfinder.netlify.app/",
+    //     description:
+    //         "App for quick navigation to a product series page on eCommerce site.",
+    //     tech: "JavaScript, HTML, CSS, SVG, Typo3 Flux.",
+    //     image: projQuickfinder,
+    //     year: "2017",
+    //     company: "Daymen",
+    // },
 
     // {
     //     name: "Math Skills Tutorial",
@@ -165,7 +177,7 @@ const projects = [
         name: "Lowepro Blog",
         url: "http://blog.lowepro.com",
         description:
-            "Wordpress theme customizations according to brands style guidelines, new blog post template, old blogs migration to new.",
+            "Customized a WordPress theme to align with the brand's style guidelines, including the creation of a new blog post template. Migrated existing blog content to the updated design for a consistent and seamless user experience.",
         tech: "Wordpress, HTML, PHP, CSS",
         image: projLoweproBlog,
         year: "2016",
@@ -174,7 +186,7 @@ const projects = [
         name: "JOBY blog",
         url: "http://blog.joby.com",
         description:
-            "Wordpress theme customizations according to brands style guidelines, new blog post template, old blogs migration to new.",
+            "Customized a WordPress theme to align with the brand's style guidelines, including the creation of a new blog post template. Migrated existing blog content to the updated design for a consistent and seamless user experience.",
         tech: "Wordpress, HTML, PHP, CSS",
         image: projJobyBlog,
         year: "2016",
