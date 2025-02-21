10a68bb80778ca645479a39ec32e647d74ae9777
Irina Kramer <irakramer@gmail.com>
2025-02-20 23:06:38 -0800

    Updates to homepage

--- src/data/services.js
+++ src/data/services.js
@@ -4,21 +4,21 @@ import support from "../icons/services/web_link.png";
 
 const services = [
     {
-        name: "Ecommerce Development (Shopify, WooCommerce, Wix)",
+        name: "Design & Build Website for Your Business",
         description:
-            "Build new ecommerce websites using Shopify, WooCommerce (WordPress), or Wix. Enhance and optimize existing stores with new features and improvements.",
+            "Create a user-friendly, impactful website tailored to your goals. Help with brainstorming, research, and growth strategies. Build a structured sitemap, repurpose existing content, and design a wireframe with key elements like contact forms, image galleries, blogs, and social media integrations. Deliver a fully responsive homepage and all non-eCommerce pages (About, Contact, etc.).",
         icon: webpage,
     },
     {
-        name: "SEO & Google Shopping Integration",
+        name: "Set Up & Optimize Your Online Store",
         description:
-            "Implement basic SEO best practices, set up Google Search Console and Google Merchant to enable your products in Google Shopping tab.",
+            "Add eCommerce functionality to your site, including product categorization, layout design, and initial product uploads. Configure payment methods, shipping options, taxes, and automated customer emails. Work primarily with Shopify and WooCommerce, with experience in Wix, Magento, and Squarespace.",
         icon: ecommerce,
     },
     {
-        name: "Web Hosting & Ongoing Support",
+        name: "Enhance Your Existing Store",
         description:
-            "Provide monthly hosting for WordPress/WooCommerce sites, along with regular app updates and support as needed.",
+            "Improve product organization with better categorization, filtering, color swatches, and multiple product page layouts. Research and integrate apps and plugins to enhance functionality and streamline operations. Apply SEO best practices and set up Google Search Console and Google Merchant to boost visibility and enable product listings in Google Shopping.",
         icon: support,
     },
 ];
